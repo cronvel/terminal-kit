b62cefc247a823bdf88149c0901f72854bda6514
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2014-11-13 18:10:49 +0100

    .getPalette() request from the terminal (if supported) the current 16 colors palette in use. .setColor() set a color register to RGB .setPalette() set the 16 colors palette, it is possible to specify a color name

--- test/256colors-test.js
+++ test/256colors-test.js
@@ -30,68 +30,70 @@
 
 require( '../lib/terminal.js' ).getDetectedTerminal( function( error , term ) {
 	
-	var i , r , g , b ;
-	
-	term.bold( '\n=== 256 colors register test ===\n\n' ) ;
-	
-	for ( i = 0 ; i <= 255 ; i ++ ) { if ( ! ( i % 70 ) ) term( '\n' ) ; term.color256( i , "*" ) ; }
-	term.styleReset( '\n' ) ;
-	for ( i = 0 ; i <= 255 ; i ++ ) { if ( ! ( i % 70 ) ) term( '\n' ) ; term.bgColor256( i , " " ) ; }
-	term.styleReset( '\n' ) ;
-	    
-	
-	
-	term.bold( '\n=== 256 colors 26 shades of gray test ===\n\n' ) ;
-	
-	for ( i = 0 ; i <= 25 ; i ++ ) { term.colorGrayscale( i * 255 / 25 , "*" ) ; }
-	term.styleReset( '\n' ) ;
-	for ( i = 0 ; i <= 25 ; i ++ ) { term.bgColorGrayscale( i * 255 / 25 , " " ) ; }
-	term.styleReset( '\n' ) ;
-	
-	
-	
-	term.bold( '\n=== 256 colors RGB 6x6x6 color cube test ===\n\n' ) ;
-	
-	for ( g = 0 ; g <= 5; g ++ )
-	{
-		for ( r = 0 ; r <= 5 ; r ++ )
+	//term.getPalette( function( error , palette ) {
+		
+		var i , r , g , b ;
+		
+		term.bold( '\n=== 256 colors register test ===\n\n' ) ;
+		
+		for ( i = 0 ; i <= 255 ; i ++ ) { if ( ! ( i % 70 ) ) term( '\n' ) ; term.color256( i , "*" ) ; }
+		term.styleReset( '\n' ) ;
+		for ( i = 0 ; i <= 255 ; i ++ ) { if ( ! ( i % 70 ) ) term( '\n' ) ; term.bgColor256( i , " " ) ; }
+		term.styleReset( '\n' ) ;
+			
+		
+		
+		term.bold( '\n=== 256 colors 26 shades of gray test ===\n\n' ) ;
+		
+		for ( i = 0 ; i <= 25 ; i ++ ) { term.colorGrayscale( i * 255 / 25 , "*" ) ; }
+		term.styleReset( '\n' ) ;
+		for ( i = 0 ; i <= 25 ; i ++ ) { term.bgColorGrayscale( i * 255 / 25 , " " ) ; }
+		term.styleReset( '\n' ) ;
+		
+		
+		
+		term.bold( '\n=== 256 colors RGB 6x6x6 color cube test ===\n\n' ) ;
+		
+		for ( g = 0 ; g <= 5; g ++ )
 		{
-			for ( b = 0 ; b <= 5 ; b ++ )
+			for ( r = 0 ; r <= 5 ; r ++ )
 			{
-				term.colorRgb( r * 255 / 5 , g * 255 / 5 , b * 255 / 5 , "*" ) ;
+				for ( b = 0 ; b <= 5 ; b ++ )
+				{
+					term.colorRgb( r * 255 / 5 , g * 255 / 5 , b * 255 / 5 , "*" ) ;
+				}
+				
+				term( ' ' ) ;
 			}
 			
-			term( ' ' ) ;
+			term.styleReset( '\n' ) ;
 		}
 		
 		term.styleReset( '\n' ) ;
-	}
-	
-	term.styleReset( '\n' ) ;
-	
-	
-	for ( g = 0 ; g <= 5; g ++ )
-	{
-		for ( r = 0 ; r <= 5 ; r ++ )
+		
+		
+		for ( g = 0 ; g <= 5; g ++ )
 		{
-			for ( b = 0 ; b <= 5 ; b ++ )
+			for ( r = 0 ; r <= 5 ; r ++ )
 			{
-				term.bgColorRgb( r * 255 / 5 , g * 255 / 5 , b * 255 / 5 , "  " ) ;
+				for ( b = 0 ; b <= 5 ; b ++ )
+				{
+					term.bgColorRgb( r * 255 / 5 , g * 255 / 5 , b * 255 / 5 , "  " ) ;
+				}
+				
+				term( ' ' ) ;
 			}
 			
-			term( ' ' ) ;
+			term.styleReset( '\n' ) ;
 		}
 		
 		term.styleReset( '\n' ) ;
-	}
-    
-	term.styleReset( '\n' ) ;
-	
-	
-	// Reset before exiting...
-	
-	term.styleReset( '\n' ) ;
-	term( 'Reset...\n' ) ;
-	
+		
+		
+		// Reset before exiting...
+		
+		term.styleReset( '\n' ) ;
+		term( 'Reset...\n' ) ;
+	//} ) ;	
 } ) ;
 
