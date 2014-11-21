534facfa72ec1d923565e9c705e4c3c295489791
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2014-11-21 19:36:09 +0100

    WIP screenBuffer

---
+++ test/screen-buffer-test.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+/*
+	The Cedric's Swiss Knife (CSK) - CSK terminal toolbox test suite
+	
+	Copyright (c) 2009 - 2014 Cédric Ronvel 
+	
+	The MIT License (MIT)
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+/* jshint unused:false */
+
+
+
+require( '../lib/terminal.js' ).getDetectedTerminal( function( error , term ) {
+	
+	var moved = 0 ;
+	
+	function moveRedraw()
+	{
+		//buffer.redrawChars() ;
+		buffer.redraw() ;
+		buffer.offsetX ++ ;
+		if ( moved ++ < 20 ) { setTimeout( moveRedraw , 100 ) ; }
+		else { term.fullscreen( false ) ; }
+	}
+	
+	term.fullscreen() ;
+	buffer = term.createScreenBuffer( { width: 10 , height: 10 } ) ;
+	//buffer.moveTo( 4 , 7 , 'toto' ) ;
+	buffer.moveTo.red( 4 , 7 , 'toto' ) ;
+	//buffer.dumpChars() ;
+	moveRedraw() ;
+} ) ;
+
+
+
+
