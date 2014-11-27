d26ac91e8ad64b57283bd427031b1749030fb31e
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2014-11-27 11:01:14 +0100

    screenbuffer-rect-test.js fixed

--- test/screenbuffer-rect-test.js
+++ test/screenbuffer-rect-test.js
@@ -47,6 +47,8 @@ describe( "ScreenBuffer.Rect" , function() {
 			ymin: 1 ,
 			xmax: term.width ,
 			ymax: term.height ,
+			width: term.width ,
+			height: term.height ,
 			isNull: false
 		} ) ;
 		
@@ -59,6 +61,8 @@ describe( "ScreenBuffer.Rect" , function() {
 			ymin: 2 ,
 			xmax: 3 ,
 			ymax: 4 ,
+			width: 3 ,
+			height: 3 ,
 			isNull: false
 		} ) ;
 		
@@ -72,44 +76,44 @@ describe( "ScreenBuffer.Rect" , function() {
 		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
 		srcRect.clip( dstRect , 0 , 0 , true ) ;
 		
-		expect( dstRect ).to.eql( { xmin: 10, ymin: 20, xmax: 25, ymax: 40 , isNull: false } ) ;
-		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 25, ymax: 40 , isNull: false } ) ;
+		expect( dstRect ).to.eql( { xmin: 10, ymin: 20, xmax: 25, ymax: 40 , width: 16 , height: 21 , isNull: false } ) ;
+		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 25, ymax: 40 , width: 16 , height: 21 , isNull: false } ) ;
 		
 		
-		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 , isNull: false } ) ;
-		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
+		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 } ) ;
+		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 } ) ;
 		srcRect.clip( dstRect , 5 , 0 , true ) ;
 		
-		expect( dstRect ).to.eql( { xmin: 15, ymin: 20, xmax: 25, ymax: 40 , isNull: false } ) ;
-		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 20, ymax: 40 , isNull: false } ) ;
+		expect( dstRect ).to.eql( { xmin: 15, ymin: 20, xmax: 25, ymax: 40 , width: 11 , height: 21 , isNull: false } ) ;
+		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 20, ymax: 40 , width: 11 , height: 21 , isNull: false } ) ;
 		
 		
-		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 , isNull: false } ) ;
-		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
+		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 } ) ;
+		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 } ) ;
 		srcRect.clip( dstRect , -8 , 0 , true ) ;
 		
-		expect( dstRect ).to.eql( { xmin: 2, ymin: 20, xmax: 22, ymax: 40 , isNull: false } ) ;
-		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 30, ymax: 40 , isNull: false } ) ;
+		expect( dstRect ).to.eql( { xmin: 2, ymin: 20, xmax: 22, ymax: 40 , width: 21 , height: 21 , isNull: false } ) ;
+		expect( srcRect ).to.eql( { xmin: 10, ymin: 20, xmax: 30, ymax: 40 , width: 21 , height: 21 , isNull: false } ) ;
 		
 		
-		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 , isNull: false } ) ;
-		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
+		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 } ) ;
+		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 } ) ;
 		srcRect.clip( dstRect , -31 , 0 , true ) ;
 		
 		expect( dstRect.isNull ).to.be( true ) ;
 		expect( srcRect.isNull ).to.be( true ) ;
 		
 		
-		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 , isNull: false } ) ;
-		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
+		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 } ) ;
+		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 } ) ;
 		srcRect.clip( dstRect , -8 , 5 , true ) ;
 		
-		expect( dstRect ).to.eql( { xmin: 2, ymin: 20, xmax: 22, ymax: 45 , isNull: false } ) ;
-		expect( srcRect ).to.eql( { xmin: 10, ymin: 15, xmax: 30, ymax: 40 , isNull: false } ) ;
+		expect( dstRect ).to.eql( { xmin: 2, ymin: 20, xmax: 22, ymax: 45 , width: 21 , height: 26 , isNull: false } ) ;
+		expect( srcRect ).to.eql( { xmin: 10, ymin: 15, xmax: 30, ymax: 40 , width: 21 , height: 26 , isNull: false } ) ;
 		
 		
-		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 , isNull: false } ) ;
-		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 , isNull: false } ) ;
+		dstRect = Rect.create( { xmin: 0 , ymin: 20 , xmax: 25 , ymax: 45 } ) ;
+		srcRect = Rect.create( { xmin: 10 , ymin: 10 , xmax: 30 , ymax: 40 } ) ;
 		srcRect.clip( dstRect , 0 , -21 , true ) ;
 		
 		expect( dstRect.isNull ).to.be( true ) ;
