8ea80a9484ba7518228a6aa19beed35e7f522636
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-05-04 15:56:50 +0200

    New method: .singleLineMenu(), which creates a simple menu, the user can select item with arrow keys and RETURN/ENTER.

---
+++ sample/single-line-menu.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+/*
+	The Cedric's Swiss Knife (CSK) - CSK terminal toolbox test suite
+	
+	Copyright (c) 2009 - 2015 Cédric Ronvel 
+	
+	The MIT License (MIT)
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+/* jshint unused:false */
+
+
+
+require( '../lib/termkit.js' ).getDetectedTerminal( function( error , term ) {
+
+	function menu()
+	{
+		var items = [
+			'File' , 'Edit' , 'View' , 'History' , 'Bookmarks' , 'Tools' , 'Help'
+		] ;
+		
+		var options = {
+			//y: 1
+			selectedStyle: term.dim.blue.bgGreen
+		}
+		
+		term.singleLineMenu( items , options , function( error , index , text ) {
+			
+			if ( error )
+			{
+				term.red.bold( "\nAn error occurs: " + error + "\n" ) ;
+				terminate() ;
+				return ;
+			}
+			
+			term.green( "\n#%s selected: %s\n" , index , text ) ;
+			terminate() ;
+		} ) ;
+	}
+	
+	
+	
+	function terminate()
+	{
+		term.grabInput( false ) ;
+		// Add a 100ms delay, so the terminal will be ready when the process effectively exit, preventing bad escape sequences drop
+		setTimeout( function() { process.exit() } , 100 ) ;
+	}
+	
+	
+	term.bold.cyan( 'Select one item from the menu:' ) ;
+	menu() ; 
+} ) ;
+
+
+
+
