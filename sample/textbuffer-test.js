87a683a7d30a768444462f9976095d20d7c76ba9
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-09-01 18:44:41 +0200

    wip TextBuffer, POC: ok\!

--- sample/textbuffer-test.js
+++ sample/textbuffer-test.js
@@ -33,14 +33,97 @@ termkit = require( '../lib/termkit.js' ) ;
 term = termkit.terminal ;
 
 
-var tbuf = termkit.TextBuffer.create() ;
+function terminate()
+{
+	setTimeout( function() {
+		//term.brightBlack( 'About to exit...\n' ) ;
+		term.grabInput( false ) ;
+		term.fullscreen( false ) ;
+		term.applicationKeypad( false ) ;
+		term.beep() ;
+		
+		// Add a 100ms delay, so the terminal will be ready when the process effectively exit, preventing bad escape sequences drop
+		setTimeout( function() { process.exit() } , 100 ) ;
+	} , 100 ) ;
+} 
 
 
-tbuf.insert( "bobby bob! " ) ;
-tbuf.insert( "bobby bob! " ) ;
-tbuf.insert( "\n\nbobby\nbob! " ) ;
-console.log( tbuf.textBuffer ) ;
+
+term.fullscreen() ;
+term.moveTo( 1 , 1 ).bgWhite.blue( 'TextBuffer test   ' ).bgWhite.green( 'CTRL-C to quit' ) ;
+term.grabInput() ;
 
 
+
+term.on( 'key' , function( key , matches , data ) {
+	
+	var draw , drawCursor ;
+	
+	
+	
+	if ( data.isCharacter )
+	{
+		tbuf.insert( key ) ;
+		draw = drawCursor = true ;
+	}
+	else
+	{
+		switch ( key )
+		{
+			case 'UP' :
+				tbuf.move( 0 , -1 ) ;
+				drawCursor = true ;
+				break ;
+			case 'DOWN' :
+				tbuf.move( 0 , 1 ) ;
+				drawCursor = true ;
+				break ;
+			case 'LEFT' :
+				tbuf.move( -1 , 0 ) ;
+				drawCursor = true ;
+				break ;
+			case 'RIGHT' :
+				tbuf.move( 1 , 0 ) ;
+				drawCursor = true ;
+				break ;
+			case 'ENTER' :
+				tbuf.newLine() ;
+				draw = drawCursor = true ;
+				break ;
+			case 'CTRL_C' :
+				terminate() ;
+				break ;
+		}
+	}
+	
+	
+	if ( draw )
+	{
+		tbuf.draw() ;
+		sbuf.draw() ;
+	}
+	
+	if ( drawCursor )
+	{
+		tbuf.drawCursor() ;
+		sbuf.drawCursor() ;
+	}
+} ) ;
+
+
+
+
+
+var sbuf = termkit.ScreenBuffer.create( { dst: term , width: 20 , height: 8 , x: 2 , y: 2 } ) ;
+var tbuf = termkit.TextBuffer.create( { dst: sbuf } ) ;
+
+tbuf.draw() ;
+sbuf.draw() ;
+tbuf.drawCursor() ;
+sbuf.drawCursor() ;
+
+
+
+//console.log( '\n' , global.deb ) ;
 
 
