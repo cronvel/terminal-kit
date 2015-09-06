6d132648dc01d35bd7fb460b56a6c6664116ec4c
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-09-06 15:47:02 +0200

    New method: fileInput(): it is build on an inputField() that auto-complete and display an auto-complete menu with all files available in the current directory.

---
+++ sample/file-input-doc1.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var termkit = require( 'terminal-kit' ) ;
+var term = termkit.terminal ;
+
+term( 'Choose a file: ' ) ;
+
+term.fileInput(
+	{ baseDir: __dirname + '/../' } ,
+	//{ baseDir: '../' } ,
+	function( error , input ) {
+		if ( error )
+		{
+			term.red.bold( "\nAn error occurs: " + error + "\n" ) ;
+		}
+		else
+		{
+			term.green( "\nYour file is '%s'\n" , input ) ;
+		}
+		
+		process.exit() ;
+	}
+) ;
