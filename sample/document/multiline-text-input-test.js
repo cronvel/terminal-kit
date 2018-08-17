e91c22ae2fa9155bb0b6277052450dcf9ac9ffaf
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-08-17 15:47:24 +0200

    wip

---
+++ sample/document/multiline-text-input-test.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+/*
+	Terminal Kit
+	
+	Copyright (c) 2009 - 2018 Cédric Ronvel
+	
+	The MIT License (MIT)
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+"use strict" ;
+
+
+
+var termkit = require( '../../lib/termkit.js' ) ;
+var term = termkit.terminal ;
+
+
+
+term.clear() ;
+
+var document = term.createDocument() ;
+
+var textInput = new termkit.TextInput( {
+	parent: document ,
+	label: 'comment: ' ,
+	x: 5 ,
+	y: 10 ,
+	width: 50 ,
+	height: 5 ,
+	allowNewLine: true
+} ) ;
+
+textInput.on( 'submit' , onSubmit ) ;
+
+function onSubmit( value )
+{
+	//console.error( 'Submitted: ' , value ) ;
+	term.saveCursor() ;
+	term.moveTo.styleReset.eraseLine( 1 , 22 , 'Submitted: %s\n' , value ) ;
+	term.restoreCursor() ;
+}
+
+document.focusNext() ;
+
+
+
+term.on( 'key' , function( key ) {
+	switch( key )
+	{
+		case 'CTRL_C' :
+			term.grabInput( false ) ;
+			term.hideCursor( false ) ;
+			term.styleReset() ;
+			term.clear() ;
+			process.exit() ;
+			break ;
+	}
+} ) ;
+
