98dcad3d7be62480524866b1cedfee326d336a60
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2019-07-23 18:00:06 +0200

    Document model -- new widget: ToggleButton

---
+++ sample/document/buttons-test.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+/*
+	Terminal Kit
+
+	Copyright (c) 2009 - 2019 Cédric Ronvel
+
+	The MIT License (MIT)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+"use strict" ;
+
+
+
+var termkit = require( '../..' ) ;
+var term = termkit.terminal ;
+
+
+
+term.clear() ;
+
+var document = term.createDocument( {
+//	backgroundAttr: { bgColor: 'magenta' , dim: true } ,
+} ) ;
+
+var button1 = new termkit.Button( {
+	parent: document ,
+	content: '> button#2' ,
+	value: 'b2' ,
+	x: 10 ,
+	y: 10 ,
+} ) ;
+
+var button2 = new termkit.Button( {
+	parent: document ,
+	content: '> button#1' ,
+	value: 'b1' ,
+	x: 13 ,
+	y: 12 ,
+} ) ;
+
+var toggleButton1 = new termkit.ToggleButton( {
+	parent: document ,
+	content: 'toggle#1' ,
+	//turnedOnLeftPadding: '☑ ' , turnedOffLeftPadding: '☐ ' ,
+	turnedOnLeftPadding: '✓ ' , turnedOffLeftPadding: '✗ ' ,
+	//value: true ,
+	x: 2 ,
+	y: 2 ,
+} ) ;
+
+var toggleButton2 = new termkit.ToggleButton( {
+	parent: document ,
+	content: 'toggle#2' ,
+	key: 'myKey' ,
+	//turnedOnLeftPadding: '☑ ' , turnedOffLeftPadding: '☐ ' ,
+	turnedOnLeftPadding: '✓ ' , turnedOffLeftPadding: '✗ ' ,
+	//value: true ,
+	x: 5 ,
+	y: 5 ,
+} ) ;
+
+//container1.draw() ;
+
+button1.on( 'submit' , onSubmit ) ;
+button2.on( 'submit' , onSubmit ) ;
+toggleButton1.on( 'toggle' , onToggle ) ;
+toggleButton1.on( 'submit' , onSubmit ) ;
+toggleButton2.on( 'toggle' , onToggle ) ;
+toggleButton2.on( 'submit' , onSubmit ) ;
+
+function onSubmit( value )
+{
+	term.saveCursor() ;
+	term.moveTo.styleReset.eraseLine( 1 , 22 , 'Submitted: %J\n' , value ) ;
+	term.restoreCursor() ;
+}
+
+function onToggle( value )
+{
+	term.saveCursor() ;
+	term.moveTo.styleReset.eraseLine( 1 , 22 , 'Toggled: %J\n' , value ) ;
+	term.restoreCursor() ;
+}
+
+
+
+document.focusNext() ;
+
+
+
+term.on( 'key' , function( key ) {
+	switch( key )
+	{
+		case 'CTRL_C' :
+			term.grabInput( false ) ;
+			term.hideCursor( false ) ;
+			term.styleReset() ;
+			term.clear() ;
+			process.exit() ;
+			break ;
+	}
+} ) ;
+
+
+
