4a5116fd839a11e442f82b3de491b2f4b5134323
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2020-02-18 11:25:50 +0100

    TextBuffer word-wrapping improved and tested

--- sample/document/text-box-test.js
+++ sample/document/text-box-test.js
@@ -37,7 +37,40 @@ term.clear() ;
 
 var text ;
 text = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.' ;
-text = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the !! Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.' ;
+
+/*
+// Test forbidden splitters
+text = '' ;
+//      |                             |		<-- column limits
+text += 'Permission  is  hereby granted ! free of charge\n' ;
+text += 'Permission is  hereby granted ! free of charge\n' ;	// forbidden splitter just after the split
+//      |                             |
+text += 'Permission is hereby granted ! free of charge\n' ;	// forbidden splitter right at the split
+text += 'Permission    is    granted ! free of charge\n' ;	// forbidden splitter just before the split
+//      |                             |
+text += 'Permission   is    granted ! free of charge\n' ;
+text += '\n-- 2 spaces --\n\n' ;
+//      |                             |
+text += 'Permission  is  hereby granted  ! free of charge\n' ;
+text += 'Permission is  hereby granted  ! free of charge\n' ;
+//      |                             |
+text += 'Permission is hereby granted  ! free of charge\n' ;
+text += 'Permission    is    granted  ! free of charge\n' ;
+//      |                             |
+text += 'Permission   is    granted  ! free of charge\n' ;
+text += 'Permission   is   granted  ! free of charge\n' ;
+text += '\n-- 3 spaces --\n\n' ;
+//      |                             |
+text += 'Permission  is  hereby granted   ! free of charge\n' ;
+text += 'Permission is  hereby granted   ! free of charge\n' ;
+//      |                             |
+text += 'Permission is hereby granted   ! free of charge\n' ;
+text += 'Permission    is    granted   ! free of charge\n' ;
+//      |                             |
+text += 'Permission   is    granted   ! free of charge\n' ;
+text += 'Permission   is   granted   ! free of charge\n' ;
+text += 'Permission  is   granted   ! free of charge\n' ;
+//*/
 
 var document = term.createDocument( {
 	palette: new termkit.Palette()
