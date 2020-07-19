30a0d5889405ad02eb41dfc106c6349f0f80c620
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2020-07-19 19:56:00 +0200

    wip: Bar widget

---
+++ sample/document/bar-test.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+/*
+	Terminal Kit
+
+	Copyright (c) 2009 - 2020 Cédric Ronvel
+
+	The MIT License (MIT)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+"use strict" ;
+
+
+
+const termkit = require( '../..' ) ;
+const term = termkit.terminal ;
+
+
+
+term.clear() ;
+
+var document = term.createDocument( {
+	palette: new termkit.Palette()
+	//	backgroundAttr: { bgColor: 'magenta' , dim: true } ,
+} ) ;
+
+var bar = new termkit.Bar( {
+	parent: document ,
+	x: 2 ,
+	y: 2 ,
+	width: 30 ,
+	value: 0.2 ,
+	barChars: 'solid'
+} ) ;
+
+setTimeout( () => bar.setValue( 0.5 ) , 1000 ) ;
+
+
+
+term.on( 'key' , function( key ) {
+	switch( key )
+	{
+		case 'CTRL_C' :
+			term.grabInput( false ) ;
+			term.hideCursor( false ) ;
+			term.styleReset() ;
+			//term.clear() ;
+			term( '\n' ) ;
+			process.exit() ;
+			break ;
+	}
+} ) ;
+
