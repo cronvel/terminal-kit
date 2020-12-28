bab0ca91195ce73eba8230d166e57d7cfb4f6a9d
Cédric Ronvel <cedric.ronvel@gmail.com>
2020-12-28 12:24:13 +0100

    Make <element>.destroy() correctly pass the noDraw argument to the superclass' .destroy()

---
+++ sample/__leak_issue.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const term = require('..').terminal;
+const Promise = require( 'seventh' ) ;
+
+const rows = 3, cols = 100;
+const labels = (new Array(cols)).fill(0).map((x, i) => i);
+const data = [labels].concat((new Array(rows).fill(0)).map(x => (new Array(cols).fill(1))));
+
+term.clear();
+
+async function flood() {
+	var i , round ;
+	for ( round = 0 ;; round ++ ) {
+		term.moveTo(1, 1)( "Starting round #%i    \n" , round );
+
+		for ( i = 0 ; i < 1000 ; i ++ ) {
+			term.moveTo(1, 2);
+			term.table(data, {});
+		}
+
+		await Promise.resolveTimeout( 10000 ) ;
+	}
+}
+
+flood() ;
+
