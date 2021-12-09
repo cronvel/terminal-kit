1a96e0c2dd52a83445d22f6739ae73f07bfe16f6
Cédric Ronvel <cedric.ronvel@gmail.com>
2021-12-09 01:49:03 +0100

    wip: TextBuffer bugfix for wide char, Cell needs refacto

--- sample/spinner-test.js
+++ sample/spinner-test.js
@@ -36,6 +36,8 @@ const term = termkit.terminal ;
 async function test() {
 	var spinner = await term.spinner( 'unboxing-color' ) ;
 	term( ' Loading... ' ) ;
+	setTimeout( () => spinner.animate( false ) , 1000 ) ;
+	setTimeout( () => spinner.animate( true ) , 2000 ) ;
 	//setTimeout( () => process.exit() , 5000 ) ;
 }
 
